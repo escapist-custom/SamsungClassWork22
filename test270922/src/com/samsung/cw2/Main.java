176fe9cb7837ab755fabf636f26e2af04f5b18e6
escapist-custom <104102364+escapist-custom@users.noreply.github.com>
2022-10-01 17:22:11 +0300

    Add class work from 270922 lesson

---
+++ test270922/src/com/samsung/cw2/Main.java
@@ -0,0 +1,17 @@
+package com.samsung.cw2;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int a, b, sum;
+
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        sum = a + b;
+        System.out.println(sum);
+
+    }
+}
