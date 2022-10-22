60023e685d435dd3b8fed8b31921868bb277c6f7
escapist-custom <104102364+escapist-custom@users.noreply.github.com>
2022-10-22 17:59:35 +0300

    All homework

--- homework1.9/src/comsamsung/numberdigits3/NumberDigits.java
+++ homework1.9/src/comsamsung/numberdigits3/NumberDigits.java
@@ -8,21 +8,19 @@ public class NumberDigits {
         int n = scanner.nextInt();
         System.out.println(sumOfThirteen(n));
     }
-    public static int sumOfThirteen (int number) {
+    public static int sumOfThirteen(int number) {
         int sum = 0;
-        int ins_sum = 0;
-        int num = 0;
         for (int i = 0; i < number; i++) {
-            int j = i;
-            while (j >= 1) {
-                j /= 10;
-                ins_sum += j;
-            }
-            if (ins_sum == 13) {
-                sum++;
-            }
-            ins_sum = 0;
+            if (sumOfDigits(i) % 13 == 0) sum++;
         }
         return sum;
+    }
+    public static int sumOfDigits(int n) {
+        int s = 0;
+        while (n != 0) {
+            s += n % 10;
+            n /= 10;
+        }
+        return s;
     }
 }
