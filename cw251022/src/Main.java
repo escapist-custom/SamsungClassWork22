b87f497f95958d22fefb398a07dcf76f103daee0
escapist-custom <104102364+escapist-custom@users.noreply.github.com>
2022-10-26 15:24:39 +0300

    Class work

---
+++ cw251022/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        double[] array = new double[3];
+        for (int i = 0; i < input.length(); i++) {
+            if (input.indexOf(i) == '+') {
+            }
+        }
+        int r =4;
+        long e = (long) 0.1;
+        System.out.println(r + e);
+        double result = 0;
+
+
+        System.out.println(result);
+    }
+}
