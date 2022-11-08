ccc283ba2417dafe8571af8d0d85e806a6f28b64
Kirill Petrovskiy <104102364+escapist-custom@users.noreply.github.com>
2022-11-08 20:45:07 +0300

    Add fourth task

--- classwork/src/com/samsung/App.java
+++ classwork/src/com/samsung/App.java
@@ -4,16 +4,15 @@ import com.samsung.pets.Pet;
 
 public class App {
     public static void main(String[] args) {
-        Pet pet = new Pet("John", "Ivan", "Dog");
-        Pet pet2 = new Pet("Myrsic", "Tom", "Cat");
-        Pet pet3 = new Pet("Dori", "Jeff", "Fish");
-        Pet pet4 = new Pet("Peter", "Dima", "spider");
-        System.out.println(pet.printInfo());
-        System.out.println();
-        System.out.println(pet2.printInfo());
-        System.out.println();
-        System.out.println(pet3.printInfo());
-        System.out.println();
-        System.out.println(pet4.printInfo());
+        Pet pet = new Pet("John", "Ivan", "Dog", "27.05.2007");
+        Pet pet2 = new Pet("Myrsic", "Tom", "Cat", "12.04.2022");
+        Pet pet3 = new Pet("Dori", "Jeff", "Fish", "08.12.2023");
+        Pet pet4 = new Pet("Peter", "Dima", "spider", "23.06.2020");
+
+        pet.anotherComing("24.05.2018");
+
+        System.out.println(pet.getDateOfComing());
+
+        pet.printDatesOfComing();
     }
 }
--- classwork/src/com/samsung/pets/Pet.java
+++ classwork/src/com/samsung/pets/Pet.java
@@ -1,14 +1,25 @@
 package com.samsung.pets;
 
+import java.util.ArrayList;
+
 public class Pet {
     private String name;
     private String nameOwner;
     private String typeOfAnimal;
 
-    public Pet(String name, String nameOwner, String typeOfAnimal) {
+    private String dateOfComing;
+
+    ArrayList<String> datesOfComing = new ArrayList<String>();
+
+
+    //---------------------------------------------------------
+    // Methods
+    public Pet(String name, String nameOwner, String typeOfAnimal, String dateOfComing) {
         this.typeOfAnimal = typeOfAnimal;
         this.nameOwner = nameOwner;
         this.name = name;
+        this.dateOfComing = dateOfComing;
+        datesOfComing.add(dateOfComing);
     }
 
     public String printInfo() {
@@ -16,5 +27,25 @@ public class Pet {
                 "Owner: " + nameOwner + "\n" +
                 "Type of animal: " + typeOfAnimal;
         return printing;
+    }
+
+    // Method for another coming
+    public void anotherComing(String dayOfComing) {
+        this.dateOfComing = dayOfComing;
+        datesOfComing.add(dayOfComing);
+    }
+
+    // Printing of all comings
+    public void printDatesOfComing() {
+        for (int i = 0; i < datesOfComing.size(); i++) {
+            System.out.print(i + 1 + ") ");
+            System.out.print(datesOfComing.get(i));
+            System.out.println();
+        }
+    }
+
+    // Getter for last coming
+    public String getDateOfComing() {
+        return dateOfComing;
     }
 }
