496be9cb663fb7b932f538544f94ff27e497862f
Kirill Petrovskiy <104102364+escapist-custom@users.noreply.github.com>
2023-02-08 17:06:04 +0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,16 @@
+package com.samsumg;
+
+import com.samsumg.lesson.MyArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) {
+        List<Per> people = new ArrayList<>();
+
+    }
+    
+}
